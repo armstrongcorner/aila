977f9810b76e4a538a6e3fbdc2a2c31ecf9707e2
armstrongcorner <armstrongcorner@163.com>
2023-06-27 11:23:32 +0800

    Update chat_content.dart

--- lib/v/chat/chat_content.dart
+++ lib/v/chat/chat_content.dart
@@ -99,9 +99,6 @@ class ChatContent extends HookConsumerWidget {
                           //           "static/images/chat_white_arrow.png")),
                           //   margin: EdgeInsets.fromLTRB(2, 16, 0, 0),
                           // ),
-                          // Container(
-                          //   child: Icon(Icons.arrow_left),
-                          // ),
                           Container(
                             decoration: const BoxDecoration(
                                 boxShadow: [
