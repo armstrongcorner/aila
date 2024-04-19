67c2e9a985d4c60cca06232fe7ab06b8fc72068a
armstrongcorner <39514373+armstrongcorner@users.noreply.github.com>
2024-04-20 11:32:20 +1200

    version update

--- apk.sh
+++ apk.sh
@@ -1,2 +1,2 @@
 flutter build apk --release --no-tree-shake-icons
-mv build/app/outputs/flutter-apk/app-release.apk ~/Desktop/.
+mv build/app/outputs/flutter-apk/app-release.apk ~/Desktop/sidu-release.apk
