46c77e78138b12b060abf8c0912a359e37fb8239
armstrongcorner <39514373+armstrongcorner@users.noreply.github.com>
2024-04-23 13:37:51 +1200

    add upload to testflight

---
+++ ipa_upload_to_testflight.sh
@@ -0,0 +1,9 @@
+if [ ! -n "$1" ] ;then
+    flutter build ipa --export-options-plist=ios/appstoreExportOptions.plist --no-tree-shake-icons
+else
+    flutter build ipa --build-number=$1 --export-options-plist=ios/appstoreExportOptions.plist --no-tree-shake-icons
+fi
+
+mv build/ios/ipa/aila.ipa ~/Desktop/sidu_appstore.ipa
+
+xcrun altool --upload-app --type ios -f ~/Desktop/sidu_appstore.ipa --apiKey 9Z7T433PM6 --apiIssuer 21962876-8727-4e44-9596-8a5c57f40a7a
